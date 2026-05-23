820ecd4ea200d73abdd02c34cbcc6cf2da94227c
hiz <kuznecovmatvej717@gmail.com>
2026-05-23 21:40:30 +0500

    version 0

---
+++ src/main/java/org/sgx/mymod/mymodClient.java
@@ -0,0 +1,10 @@
+package org.sgx.mymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mymodClient implements ClientModInitializer {
+    @Override
+    public void onInitalizeClient() {
+
+    }
+}
